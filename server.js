d50216c35302c446040eaac7b3efe9ba093f7c87
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-08-24 23:40:24 -0700

    Implement db connection

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Todo = require('./api/models/model');
+const bodyParser = require('body-parser');
+const routes = require('./api/routes/routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
+mongoose.connect(`mongodb://localhost:${port}/todo`, {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+routes(app);
 app.listen(port);
 
 console.log(`Listening to port: ${port}`);
